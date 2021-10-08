533bf14aa362427946a5bb60c4031c4655c2ac19
Nathan Rawling <nathan.rawling@hobsons.com>
2021-10-08 13:40:19 -0400

    Completed Python Lab 3

--- python/webserver.py
+++ python/webserver.py
@@ -1,9 +1,21 @@
 from flask import Flask
+from snowflake import connector
+import keyring
 
 app = Flask("my website")
 
 @app.route('/')
 def homepage():
-    return "Welcome to my website!"
+    return "Welcome to my website! My Snowflake Acct # is " + str(onerow)
+
+# Snowflake
+cnx = connector.connect(
+    account='xt70683.us-central1.gcp',
+    user='nathanrawling',
+    password=keyring.get_password('snowflake','badge3')
+)
+cur = cnx.cursor()
+cur.execute("SELECT current_account()")
+onerow = cur.fetchone()
 
 app.run()
