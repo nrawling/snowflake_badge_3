c0602c0c2a2f2766e65cbeee0f41cf7a5ed11e17
Nathan Rawling <nathan.rawling@hobsons.com>
2021-10-11 10:48:11 -0400

    Completed Python Lab 6

--- python/webserver.py
+++ python/webserver.py
@@ -1,21 +1,28 @@
-from flask import Flask
+from flask import Flask, render_template
 from snowflake import connector
 import keyring
+import pandas as pd
 
 app = Flask("my website")
 
 @app.route('/')
 def homepage():
-    return "Welcome to my website! My Snowflake Acct # is " + str(onerow)
+    return render_template("index.html", dfhtml=dfhtml)
 
 # Snowflake
 cnx = connector.connect(
     account='xt70683.us-central1.gcp',
     user='nathanrawling',
-    password=keyring.get_password('snowflake','badge3')
+    password=keyring.get_password('snowflake','badge3'),
+    warehouse='COMPUTE_WH',
+    database='DEMO_DB',
+    schema='PUBLIC'
 )
 cur = cnx.cursor()
-cur.execute("SELECT current_account()")
-onerow = cur.fetchone()
+cur.execute("SELECT * FROM COLORS")
+rows=pd.DataFrame(cur.fetchall(),columns=['Color UID','Color Name'])
+
+# test dataframe as html
+dfhtml = rows.to_html()
 
 app.run()
